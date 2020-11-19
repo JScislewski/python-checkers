37badba5bf8b248daafe9a6fedf125a94cf02e99
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-19 04:17:01 +0100

    Add game loop

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+
+
+def main():
+    run = True
+
+    while run:
+        pass
+
+
+main()
