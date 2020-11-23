1eb4b58f8324bedca52e34f368b4c8b47870ef27
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-23 15:58:32 +0100

    Add event loop

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 from checkers.constants import WIDTH, HEIGHT
 
 FPS = 60
-
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
@@ -13,7 +12,15 @@ def main():
 
     while run:
         clock.tick(FPS)
-        pass
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quit()
 
 
 main()
