2c69bf5422e5a8a80fbb85f910737cae81f67933
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-23 15:53:32 +0100

    Add clock to game loop

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
 from checkers.constants import WIDTH, HEIGHT
 
+FPS = 60
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
         pass
 
 
